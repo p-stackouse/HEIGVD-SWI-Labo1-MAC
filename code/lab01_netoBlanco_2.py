6fe0573d4198984e49143a7a1bf5d50c24f9871c
p-stackouse <patrick.neto@protonmail.com>
2019-03-20 21:55:19 +0100

    Finishing exercise 2b and commenting code (ready to test and release)

--- code/lab01_netoBlanco_2.py
+++ code/lab01_netoBlanco_2.py
@@ -1,25 +1,33 @@
 #! /usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+
 ######################################################################################################################
 # Fichier: lab01_netoBlanco_2.py
-# Description:  Script de découverte de réseau, capable d'afficher les réseaux 802.11 environnant (SSID, Fabricant, MAC)
+# Description:  Script de découverte de dispositifs résau, capable d'afficher leurs infos (SSID recherché, Fabricant,
+# MAC), en fonction du probe request émis.
 # Auteurs: Guillaume Blanco, Patrick Neto
 # Date: 18.03.2019
 ######################################################################################################################
 import scapy.all as scapy
 import requests
 
-#Nom de l'interface réseau utilisée pour monitorer
-INTERFACE_WIFI = "en0"
+# Nom de l'interface réseau utilisée pour monitorer
+INTERFACE_WIFI = "wlan0mon"
 
-#Adresse de l'API qui résout les mac address en nom de fabricant
+#  Adresse de l'API qui traduit les MAC address en nom du fabricant du dispositif
 API_MAC_URL = "https://api.macvendors.com/"
 
+# Tableaux qui stockent respectivement les noms de fabricants des différentes MAC adresses, ainsi que les SSID recherchés
+#dans le probe
 macAddresses = {}
 macSsids = {}
 
+##############################################################################
+## Retourne le fabriquant d'un dispositif, à l'aide de sa MAC adresse
+## In: mac - MAC adresse du dispositif
+##############################################################################
 def macToVendor(mac):
-    curUrl = API_MAC_URL + mac.upper().replace(":","-") #Formattage de l'adresse de l'API pour requête
+    curUrl = API_MAC_URL + mac.upper().replace(":","-") # Formattage de l'URL pour la requête sur l'API
     req = requests.get(url=curUrl)
 
     vendor = "Fabricant inconnu"
@@ -31,34 +39,39 @@ def macToVendor(mac):
     return vendor
 
 ##############################################################################
-## Recupère les informations du paquet récupéré
+## Recupère et affiche les informations du paquet récupéré (MAC adresse, probe,
+## SSID)
 ## In: packet - paquet qui a été récupéré
 ##############################################################################
 def detectProbeRequest(packet):
         # Vérifie si le paquet contient les informations nécessaire à lister les dispositifs et leurs probe request ssid
-        if (hasattr(packet, 'type') and packet.type == 0 and packet.subtype == 4 and hasattr(packet, 'info') and packet.info):
+        if (packet.haslayer(scapy.Dot11) and hasattr(packet, 'type') and packet.type == 0 and packet.subtype == 4 \
+            and hasattr(packet, 'info') and packet.info):
             # Vérifie si l'adresse MAC du dispositif n'est pas encore enregistrée
             if(packet.addr2 not in macAddresses):
                 macAddresses[packet.addr2] = macToVendor(packet.addr2)
 
+            # Vérifier que la liste de SSID recherchés par une MAC adresse n'est pas encore initialisée
             if(packet.addr2 not in macSsids):
                 macSsids[packet.addr2] = []
+
+            # Vérifier le SSID contenu dans le paquet n'est pas
             if (packet.info not in macSsids[packet.addr2]):
                 macSsids[packet.addr2].append(packet.info)
-            printCliensInfos()
+            printCliensInfos()# Affiche les informations
 
-
+##############################################################################
+## Affiche les informations précédemment récupérées par le sniffer de probe
+## request
+## SSID)
+## In: packet - paquet qui a été récupéré
+##############################################################################
 def printCliensInfos():
-    print(chr(27) + "[2J")
+    print(chr(27) + "[2J") # Permet de raffraîchir l'affichage en console
     for mac in macAddresses:
-        print(mac + " (" + macAddresses[mac] + ") - " + "".join([str(" [" + x + "]").encode("utf-8") for x in macSsids[mac]]))
+        print(mac + " (" + macAddresses[mac] + ") - " + "".join([str(" [" + x + "]").decode("utf-8") for x in \
+              macSsids[mac]]))
 
-def printNetworks(mac):
-    for ssid in macSsids[mac]:
-        print(str(ssid).decode("utf-8") + ", ")
-
-
-# On sniff le reseau avec l interface wlan0mon et on applique la fonction detectMac pour chaque paquet
-pckt = scapy.sniff(iface=INTERFACE_WIFI, monitor="true", prn=detectProbeRequest) # Ajouter l'argument "monitor=true" dans le cas de l'utilisation d'un Mac
-
-
+# Sniffing de paquets 802.11 et passage en paramètre d'une fonction de traitement de paquets
+scapy.sniff(iface=INTERFACE_WIFI,  prn=detectProbeRequest) # Ajouter l'argument "monitor=true" dans le cas de
+# l'utilisation d'un Mac
